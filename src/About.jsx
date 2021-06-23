a311df9310f96c89492de4739c10ded212740a90
sadeghpro <sadeghmohamadnia@yahoo.com>
2021-06-23 23:18:10 +0430

    Add react router and change app

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {withRouter} from 'react-router-dom';
+
+class About extends React.Component {
+
+    render() {
+        return (
+            <>
+                main about
+            </>
+        );
+    }
+}
+
+export default withRouter(About);
